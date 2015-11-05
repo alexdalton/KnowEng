d13c38c43458caf6251f83616e550ed18edac7da
Alex Dalton <alexmdalton@gmail.com>
2015-11-04 18:40:15 -0600

    stuff
    let's not commit csvs

--- src/dataGrabber.py
+++ src/dataGrabber.py
@@ -1,20 +1,31 @@
+class dataFileDescriptor:
+    def __init__(self, fileName, filterRule = lambda x: True, geneIdx = 1, dataIdx = 0, splitCriteria = None):
+        self.fileName = fileName
+        self.filterRule = filterRule
+        self.geneIdx = geneIdx
+        self.dataIdx = dataIdx
+        self.splitCriteria = None
+
 class dataGrabber:
 
-    def __init__(self, filtMax=None, filtMin=None):
+    def __init__(self):
         self.data = {}
         self.dataIndices = {}
         self.dataHistogram = {}
-        self.filtMax = filtMax
-        self.filtMin = filtMin
 
-    def getData(self, labelFileName, dataFileName, geneIdx, dataIdx, splitCriteria = None):
-        self._getDataHistogram(dataFileName, dataIdx, splitCriteria)
-        self._readData(dataFileName, geneIdx, dataIdx, splitCriteria)
-        wantedGeneIds = open(labelFileName, "r").read().split()
+    def getData(self, positiveLabelsFileName, dataFileDescriptors):
+        self._getDataHistogram(dataFileDescriptors)
+        self._readData(dataFileDescriptors)
+
+        wantedGeneIds = open(positiveLabelsFileName, "r").read().split()
         labelDict = {}
+        count = 0
         for wantedGeneId in wantedGeneIds:
             if wantedGeneId in self.data:
                 labelDict[wantedGeneId] = 1
+            if wantedGeneId not in self.data:
+                count += 1
+        print("Missing {0} geneIDs from {1} in data file set".format(count, positiveLabelsFileName))
         for geneID in self.data.iterkeys():
             if geneID not in labelDict:
                 labelDict[geneID] = 0
@@ -22,10 +33,9 @@ class dataGrabber:
 
     def convertToCSV(self, featureDict, labelDict, fileName):
         csvFile = open(fileName, "w")
-        featureLength = len(featureDict.values()[0])
-        featureNames = []
-        for i in range(0, featureLength):
-            featureNames.append("F{0}".format(i))
+        featureNames = ["GeneID"] + [""] * len(self.dataIndices)
+        for edgeName, index in self.dataIndices.iteritems():
+            featureNames[index + 1] = edgeName
         featureNames.append("Class")
         csvFile.write(str(featureNames).strip("[]").replace("'", "") + '\n')
         for key, featureVector in featureDict.iteritems():
@@ -33,44 +43,66 @@ class dataGrabber:
                 label = "POSITIVE"
             else:
                 label = "NEGATIVE"
-            csvFile.write(str(featureVector).strip("[]").replace("1", "TRUE").replace("0", "FALSE") + ", " + label + '\n')
+            outString = str(key) + ", " + str(featureVector).strip("[]").replace("1", "TRUE").replace("0", "FALSE") + ", " + str(label)
+            csvFile.write(outString + '\n')
         csvFile.close()
 
-    def _readData(self, fileName, geneIdx, dataIdx, splitCriteria):
-        dataFile = open(fileName, "r")
-        dataDims = self._getDataDimensionality(fileName, dataIdx, splitCriteria)
-        for line in dataFile:
-            items = line.split(splitCriteria)
-            data = items[dataIdx]
-            geneId = items[geneIdx]
-            if geneId not in self.data:
-                self.data[geneId] = [0] * dataDims
-            if data in self.dataIndices:
-                self.data[geneId][self.dataIndices[data]] = 1
-        dataFile.close()
+    def _readData(self, dataFileDescriptors):
+        dataDims = self._getDataDimensionality(dataFileDescriptors)
+        for fileDescriptor in dataFileDescriptors:
+            dataFile = open(fileDescriptor.fileName, "r")
+            for line in dataFile:
+                items = line.split(fileDescriptor.splitCriteria)
+                data = items[fileDescriptor.dataIdx]
+                geneId = items[fileDescriptor.geneIdx]
+                if geneId not in self.data:
+                    self.data[geneId] = [0] * dataDims
+                if data in self.dataIndices:
+                    self.data[geneId][self.dataIndices[data]] = 1
+            dataFile.close()
 
-    def _getDataHistogram(self, fileName, dataIdx, splitCriteria):
-        dataFile = open(fileName, "r")
-        for line in dataFile:
-            items = line.split(splitCriteria)
-            data = items[dataIdx]
-            if data not in self.dataHistogram:
-                self.dataHistogram[data] = 0
-            self.dataHistogram[data] += 1
-        dataFile.close()
+    def _getDataHistogram(self, dataFileDescriptors):
+        for fileDescriptor in dataFileDescriptors:
+            dataFile = open(fileDescriptor.fileName, "r")
+            for line in dataFile:
+                items = line.split(fileDescriptor.splitCriteria)
+                data = items[fileDescriptor.dataIdx]
+                geneId = items[fileDescriptor.geneIdx]
+                if data not in self.dataHistogram:
+                    self.dataHistogram[data] = set()
+                self.dataHistogram[data].add(geneId)
+            dataFile.close()
+        for dataTypeName, geneSet in self.dataHistogram.iteritems():
+            self.dataHistogram[dataTypeName] = len(geneSet)
 
-    def _getDataDimensionality(self, fileName, dataIdx, splitCriteria):
-        dataFile = open(fileName, "r")
+    def _getDataDimensionality(self, dataFileDescriptors):
         curIndex = 0
-        for line in dataFile:
-            items = line.split(splitCriteria)
-            data = items[dataIdx]
-            if data not in self.dataIndices:
-                if (self.filtMin == None) or (self.filtMax == None):
-                    self.dataIndices[data] = curIndex
-                    curIndex = curIndex + 1
-                elif (self.dataHistogram[data] >= self.filtMin) and (self.dataHistogram[data] <= self.filtMax):
-                    self.dataIndices[data] = curIndex
-                    curIndex = curIndex + 1
-        dataFile.close()
+        x = ["go_small_molecule_metabolic_process",
+             "go_glucose_metabolic_process",
+             "go_negative_regulation_of_cell_grow",
+             "go_protein_homodimerization_activit",
+             "go_liver_development",
+             "go_enzyme_binding",
+             "go_mitochondrion",
+             "go_identical_protein_binding",
+             "go_negative_regulation_of_neuron_ap",
+             "go_autophagosome",
+             "go_cellular_nitrogen_compound_metab",
+             "go_electron_carrier_activity",
+             "go_mitochondrial_matrix",
+             "go_iron_ion_binding",
+             "go_cell_proliferation",
+             "go_condensed_chromosome_kinetochore",
+             "go_perinuclear_region_of_cytoplasm"]
+        for fileDescriptor in dataFileDescriptors:
+            dataFile = open(fileDescriptor.fileName, "r")
+            for line in dataFile:
+                items = line.split(fileDescriptor.splitCriteria)
+                data = items[fileDescriptor.dataIdx]
+                if data not in self.dataIndices:
+                    #if fileDescriptor.filterRule(self.dataHistogram[data]):
+                    if data in x:
+                        self.dataIndices[data] = curIndex
+                        curIndex = curIndex + 1
+            dataFile.close()
         return curIndex
--- src/run.py
+++ src/run.py
@@ -1,22 +1,20 @@
-from dataGrabber import dataGrabber
+from dataGrabber import dataGrabber, dataFileDescriptor
 from PEBL import PEBL
 from featureSelection import featureSelection
 from centroid import Centroid
 from helpers import helpers
 
-labelFile = "/home/alex/KnowEng/data/LEE_LIVER_CANCER_ACOX1.CB.txt"
-dataFile1 = "/home/alex/KnowEng/data/ENSG.kegg_pathway.txt"
-dataFile2 = "/home/alex/KnowEng/data/ENSG.go_%_evid.txt"
-
-#dataRetriever = dataGrabber(2000, 20)
-#(featureVectorDict1, labelDict1, dataIndices1) = dataRetriever.getData(labelFile, dataFile2, 1, 0)
+#labelFile = "/home/alex/KnowEng/data/LEE_LIVER_CANCER_ACOX1.CB.txt"
+breastCancerLabelFile = "/home/alex/KnowEng/data/VANTVEER_BREAST_CANCER_ESR1.CB.txt"
+keggDataFile = dataFileDescriptor("/home/alex/KnowEng/data/ENSG.kegg_pathway.txt")
+goDataFile = dataFileDescriptor("/home/alex/KnowEng/data/ENSG.go_%_evid.txt",
+                                lambda x: x >= 40 and x <= 2000)
 
 
 dataRetriever = dataGrabber()
-(featureVectorDict2, labelDict2, dataIndices2) = dataRetriever.getData(labelFile, dataFile1, 1, 0)
+(featureVectorDict, labelDict, dataIndices) = dataRetriever.getData(breastCancerLabelFile, [goDataFile])
 
-dataRetriever.convertToCSV(featureVectorDict2, labelDict2, "out4.csv")
-
+#dataRetriever.convertToCSV(featureVectorDict, labelDict, "BC_GO_FILT_RIPPER.csv")
 
 # newFeatureDict = {}
 # newLabelDict = {}
@@ -33,16 +31,18 @@ dataRetriever.convertToCSV(featureVectorDict2, labelDict2, "out4.csv")
 #             unlabeledCount += 1
 
 
-# posX = helpers().getFeaturesByLabel(newFeatureDict, newLabelDict, 1)
-# unlabX = helpers().getFeaturesByLabel(newFeatureDict, newLabelDict, 0)
+# posX = helpers().getFeaturesByLabel(featureVectorDict, labelDict, 1)
+unlabX = helpers().getFeaturesByLabel(featureVectorDict, labelDict, 0)
+#
+#
 #
 # centroidPosX = Centroid().getCentroid(posX)
 # negIndices = Centroid().getNFarthestPoints(unlabX, centroidPosX, len(posX))
 #
 # negX = []
 # for i in negIndices:
-#     negX.append(unlabX[i])
-#
+#      negX.append(unlabX[i])
+
 # newnewFeatureDict = {}
 # for k, v in newLabelDict.iteritems():
 #     if v == 1:
@@ -53,25 +53,25 @@ dataRetriever.convertToCSV(featureVectorDict2, labelDict2, "out4.csv")
 
 # dataRetriever.convertToCSV(newFeatureDict, newLabelDict, "out2.csv")
 
-#
-# crossVal = []
-# delKeys = []
-#
-# count = 0
-# total = 0
-# for k, v in labelDict.iteritems():
-#     if v == 1:
-#         count += 1
-#         total += 1
-#     if count == 4:
-#         count = 0
-#         crossVal.append(featureVectorDict[k])
-#         delKeys.append(k)
-#         del featureVectorDict[k]
-# for key in delKeys:
-#     del labelDict[key]
-#
-# x = PEBL(verbose=True)
+
+crossVal = []
+delKeys = []
+
+count = 0
+total = 0
+for k, v in labelDict.iteritems():
+    if v == 1:
+        count += 1
+        total += 1
+    if count == 4:
+        count = 0
+        crossVal.append(featureVectorDict[k])
+        delKeys.append(k)
+        del featureVectorDict[k]
+for key in delKeys:
+    del labelDict[key]
+
+x = PEBL(verbose=True)
 
 # print("no gamma")
 # x.train(featureVectorDict, labelDict)
@@ -81,11 +81,10 @@ dataRetriever.convertToCSV(featureVectorDict2, labelDict2, "out4.csv")
 # x.train(featureVectorDict, labelDict, gamma=.8)
 # x.score(crossVal + unlabX, [1] * len(crossVal) + [0] * len(unlabX))
 #
-# print(".2 gamma")
-# print("number of hidden positives: {0}".format(len(crossVal)))
-# print("Data size: {0} x {1}".format(len(featureVectorDict.values()[0]), len(featureVectorDict)))
-# x.train(featureVectorDict, labelDict, gamma=.2)
-# x.score(crossVal + unlabX, [1] * len(crossVal) + [0] * len(unlabX))
+print("number of hidden positives: {0}".format(len(crossVal)))
+print("Data size: {0} x {1}".format(len(featureVectorDict.values()[0]), len(featureVectorDict)))
+x.train(featureVectorDict, labelDict, kernel="poly", probability=True, class_weight="auto", gamma=.2)
+x.score(crossVal + unlabX, [1] * len(crossVal) + [0] * len(unlabX))
 #
 # print("linear")
 # x.train(featureVectorDict, labelDict, kernel="linear")
