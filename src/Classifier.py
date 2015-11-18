e6438abecd58131c13d2b1591e73d1196c7ba929
Alex Dalton <alexmdalton@gmail.com>
2015-11-17 22:15:44 -0600

    Made things all nice

--- src/Classifier.py
+++ src/Classifier.py
@@ -3,39 +3,110 @@ from centroid import Centroid
 from helpers import helpers
 import time
 from tabulate import tabulate
+from random import sample
 
 
 class Classifier:
 
     def __init__(self, logger, verbose=False):
+        # class variables to initialize general classifier
         self.logger = logger
         self.verbose = verbose
-        self.trainedSVM = None
         self.helperObj = helpers(logger)
 
-    def score(self, X, y):
-        if self.trainedSVM == None:
+        # class variables to initialize once a classificion model is specified
+        # (i.e. initialized after a train method has been called)
+        self.kCrossValPos = 1
+        self.kCrossValNeg = 1
+        self.totalExamples = 0
+        self.featureDims = 0
+        self.dict_X = {}
+        self.dict_y = {}
+        self.allPosExampleKeys = []
+        self.allNegExampleKeys = []
+        self.numAllPosExamples = 0
+        self.numAllNegExamples = 0
+        self.hidden_X = []
+        self.hidden_y = []
+        self.hiddenPosExampleKeys = []
+        self.hiddenNegExampleKeys = []
+        self.numHiddenPosExamples = 0
+        self.numHiddenNegExamples = 0
+
+        # holds the trained classifier after training
+        self.trainedClassifier = None
+
+    def _initNewClassificationModel(self, dict_X, dict_y, kCrossValPos, kCrossValNeg):
+        self.dict_X = dict_X
+        self.dict_y = dict_y
+        self.kCrossValPos = kCrossValPos
+        self.kCrossValNeg = kCrossValNeg
+        self.totalExamples = len(dict_X)
+        self.featureDims = len(dict_X.values()[0])
+        (self.allPosExampleKeys, self.allNegExampleKeys) = self.helperObj.getLabelSets(dict_y)
+        self.numAllPosExamples = len(self.allPosExampleKeys)
+        self.numAllNegExamples = len(self.allNegExampleKeys)
+        self._hideSamples()
+
+    def _hideSamples(self):
+        if self.kCrossValPos > 1:
+            self.hiddenPosExampleKeys = sample(self.allPosExampleKeys, self.numAllPosExamples / self.kCrossValPos)
+            self.numHiddenPosExamples = len(self.hiddenPosExampleKeys)
+            self.logger.log("{0}-fold cross-validation on positive examples".format(self.kCrossValPos))
+
+        if self.kCrossValNeg > 1:
+            self.hiddenNegExampleKeys = sample(self.allNegExampleKeys, self.numAllNegExamples / self.kCrossValNeg)
+            self.numHiddenNegExamples = len(self.hiddenNegExampleKeys)
+            self.logger.log("{0}-fold cross-validation on negative examples".format(self.kCrossValNeg))
+
+        for exampleKey in self.hiddenPosExampleKeys + self.hiddenNegExampleKeys:
+            self.hidden_X.append(self.dict_X[exampleKey])
+            self.hidden_y.append(self.dict_y[exampleKey])
+            del self.dict_X[exampleKey]
+            del self.dict_y[exampleKey]
+
+        self.logger.log("")
+
+    def score(self):
+        if self.trainedClassifier == None:
             self.logger.log("Need to train an SVM first")
             return
-        
+
         start = time.mktime(time.localtime())
 
-        predictions = list(self.trainedSVM.predict(X))
+        predPosStr = "the hidden positive examples"
+        # If no cross validation on positive examples, add all positives to hidden X and y
+        if self.kCrossValPos <= 1:
+            predPosStr = "all the given positive training examples"
+            for posExampleKey in self.allPosExampleKeys:
+                self.hidden_X.append(self.dict_X[posExampleKey])
+                self.hidden_y.append(1)
+
+        predNegStr = "the hidden negative examples"
+        # If no cross validation on negative examples, add all negatives to hidden X and y
+        if self.kCrossValNeg <= 1:
+            predNegStr = "all the given negative training examples"
+            for negExampleKey in self.allNegExampleKeys:
+                self.hidden_X.append(self.dict_X[negExampleKey])
+                self.hidden_y.append(0)
+
+        self.logger.log("Predicting on {0} and {0}".format(predPosStr, predNegStr))
+        predictions = list(self.trainedClassifier.predict(self.hidden_X))
 
         end = time.mktime(time.localtime())
         self.logger.log("Elapsed Prediction Time: {0} minutes".format((end - start) / 60))
 
-        numPosEx = float(y.count(1))
-        numNegEx = float(y.count(0))
+        numPosEx = float(self.hidden_y.count(1))
+        numNegEx = float(self.hidden_y.count(0))
         numCorrectPos = 0.0
         numCorrectNeg = 0.0
         numPosPredictions = float(predictions.count(1))
         numNegPredictions = float(predictions.count(0))
 
         for i in range(0, len(predictions)):
-            if predictions[i] == y[i] == 1:
+            if predictions[i] == self.hidden_y[i] == 1:
                 numCorrectPos += 1.0
-            if predictions[i] == y[i] == 0:
+            if predictions[i] == self.hidden_y[i] == 0:
                 numCorrectNeg += 1.0
 
         try:
@@ -81,78 +152,83 @@ class Classifier:
             nf1 = 1
 
         headers = ["Class", "Precision", "Recall", "F1"]
-        table = [["+", pprecision, precall, pf1],
-                 ["-", nprecision, nrecall, nf1]]
+        table = [["POSITIVE", pprecision, precall, pf1],
+                 ["NEGATIVE", nprecision, nrecall, nf1]]
         self.logger.log('\n' + tabulate(table, headers=headers))
 
-        headers = ["", "Predicted +", "Predicted -"]
-        table = [["Actual +", numCorrectPos, numNegPredictions - numCorrectNeg],
-                 ["Actual -", numPosPredictions - numCorrectPos, numCorrectNeg]]
+        headers = ["", "Predicted POSITIVE", "Predicted NEGATIVE"]
+        table = [["Actual POSITIVE", numCorrectPos, numNegPredictions - numCorrectNeg],
+                 ["Actual NEGATIVE", numPosPredictions - numCorrectPos, numCorrectNeg]]
         self.logger.log('\n' + tabulate(table, headers=headers))
 
         self.logger.log("\nOverall accuracy: {0}".format(accuracy))
 
-    def printVerboseTrainInfo(self, featureDict, labelDict, positiveSet, unlabeledSet, kernel, gamma,
-                              probability, shrinking, class_weight, degree, coef0):
+    def _printVerboseTrainInfo(self, C, kernel, degree, gamma, coef0, probability, shrinking, class_weight):
         if self.verbose:
-            headers = ["Kernel", "Gamma", "Probability", "Class Weight", "Shrinking", "Degree", "Coef0"]
             degree_str = str(degree)
+            coef0_str = str(coef0)
             if gamma == 0.0:
-                gamma_str = str(1.0 / len(featureDict.values()[0]))
+                gamma_str = str(1.0 / self.featureDims)
             else:
                 gamma_str = str(gamma)
-            coef0_str = str(coef0)
+
             if kernel != "sigmoid" and kernel != "poly":
                 coef0_str = "N\A"
             if kernel != "poly":
                 degree_str = "N\A"
             if kernel != "rbf" and kernel != "poly" and kernel != "sigmoid":
                 gamma_str = "N\A"
-            table = [[kernel, gamma_str, str(probability), class_weight, str(shrinking), degree_str, coef0_str]]
-            self.logger.log(tabulate(table, headers=headers) + '\n')
-            self.logger.log("Data size: {0} x {1}".format(len(featureDict.values()[0]), len(featureDict)))
-            self.logger.log("Number of Positive Examples: {0}".format(len(positiveSet)))
-            self.logger.log("Number of Negative/Unlabeled Examples: {0}".format(len(unlabeledSet)))
 
-    def trainSVM(self, featureDict, labelDict, kernel="rbf", gamma=0.0,
+            headers = ["Kernel", "C", "Gamma", "Degree", "Coef0", "Probability", "Class Weight", "Shrinking"]
+            table = [[str(kernel), str(C), gamma_str, degree_str, coef0_str, str(probability), str(class_weight),
+                      str(shrinking)]]
+            self.logger.log(tabulate(table, headers=headers) + '\n')
+            self.logger.log("Data size: {0} x {1}".format(self.featureDims, self.totalExamples))
+            self.logger.log("Total Number of Positive Examples: {0}".format(self.numAllPosExamples))
+            self.logger.log("Number of Hidden Positive Examples: {0}".format(self.numHiddenPosExamples))
+            self.logger.log("Total Number of Negative Examples: {0}".format(self.numAllNegExamples))
+            self.logger.log("Number of Hidden Negative Examples: {0}".format(self.numHiddenNegExamples))
+
+    def trainSVM(self, dict_X, dict_y, kCrossValPos=0, kCrossValNeg=0, C=1.0, kernel="rbf", gamma=0.0,
                  probability=False, shrinking=True, class_weight="auto", degree=3, coef0=0.0):
         self.logger.log("Algorithm: SVM\n")
-        (positiveSet, unlabeledSet) = self.helperObj.getLabelSets(labelDict)
-
-        self.printVerboseTrainInfo(featureDict, labelDict, positiveSet, unlabeledSet, kernel, gamma, probability,
-                                   shrinking, class_weight, degree, coef0)
+        self._initNewClassificationModel(dict_X, dict_y, kCrossValPos, kCrossValNeg)
+        self._printVerboseTrainInfo(C, kernel, degree, gamma, coef0, probability, shrinking, class_weight)
 
         start = time.mktime(time.localtime())
 
-        labels = ([1] * len(positiveSet)) + ([0] * len(unlabeledSet))
+        positiveExampleKeys = set(self.allPosExampleKeys) - set(self.hiddenPosExampleKeys)
+        negativeExampleKeys = set(self.allNegExampleKeys) - set(self.hiddenNegExampleKeys)
 
-        featureVectors = self.helperObj.dictOfFeaturesToList(featureDict, positiveSet) + \
-                         self.helperObj.dictOfFeaturesToList(featureDict, unlabeledSet)
+        y = ([1] * len(positiveExampleKeys)) + ([0] * len(negativeExampleKeys))
 
-        svc = svm.SVC(kernel=kernel, probability=probability, shrinking=shrinking,
+        X = self.helperObj.dictOfFeaturesToList(self.dict_X, positiveExampleKeys) + \
+                         self.helperObj.dictOfFeaturesToList(self.dict_X, negativeExampleKeys)
+
+        svc = svm.SVC(C=C, kernel=kernel, probability=probability, shrinking=shrinking,
                       class_weight=class_weight, gamma=gamma, degree=degree, coef0=coef0)
 
-        svc.fit(featureVectors, labels)
+        svc.fit(X, y)
 
-        self.trainedSVM = svc
+        self.trainedClassifier = svc
         end = time.mktime(time.localtime())
         self.logger.log("Elapsed training time: {0} minutes".format((end - start) / 60))
         return svc
 
-    def trainPEBLSVM(self, featureDict, labelDict, kernel="rbf", gamma=0.0,
-              probability=False, shrinking=True, class_weight="auto", degree=3, coef0=0.0):
+    def trainPEBLSVM(self, dict_X, dict_y, kCrossValPos=0, kCrossValNeg=0, C=1.0, kernel="rbf", gamma=0.0,
+                     probability=False, shrinking=True, class_weight="auto", degree=3, coef0=0.0):
         self.logger.log("Algorithm: PEBL\n")
+        self._initNewClassificationModel(dict_X, dict_y, kCrossValPos, kCrossValNeg)
+        self._printVerboseTrainInfo(C, kernel, degree, gamma, coef0, probability, shrinking, class_weight)
 
         start = time.mktime(time.localtime())
 
-        (positiveSet, unlabeledSet) = self.helperObj.getLabelSets(labelDict)
+        positiveExampleKeys = set(self.allPosExampleKeys) - set(self.hiddenPosExampleKeys)
+        unlabeledExampleKeys = set(self.allNegExampleKeys) - set(self.hiddenNegExampleKeys)
 
-        newNegativeFeatures = self._strongNegatives(list(unlabeledSet), positiveSet, featureDict)
+        newNegativeFeatures = self._strongNegatives(list(unlabeledExampleKeys), positiveExampleKeys)
         negativeSet = set()
         iterSVM = None
-
-        self.printVerboseTrainInfo(featureDict, labelDict, positiveSet, unlabeledSet, kernel, gamma, probability,
-                                   shrinking, class_weight, degree, coef0)
 
         iterCount = 0
         while(len(newNegativeFeatures)):
@@ -160,14 +236,14 @@ class Classifier:
             negativeSet = negativeSet | newNegativeFeatures
             iterSVM = svm.SVC(kernel=kernel, probability=probability,
                               class_weight=class_weight, gamma=gamma, degree=degree, coef0=coef0)
-            labels = ([1] * len(positiveSet)) + ([0] * len(negativeSet))
-            featureVectors = self.helperObj.dictOfFeaturesToList(featureDict, positiveSet) + \
-                             self.helperObj.dictOfFeaturesToList(featureDict, negativeSet)
+            labels = ([1] * len(positiveExampleKeys)) + ([0] * len(negativeSet))
+            featureVectors = self.helperObj.dictOfFeaturesToList(self.dict_X, positiveExampleKeys) + \
+                             self.helperObj.dictOfFeaturesToList(self.dict_X, negativeSet)
             iterSVM.fit(featureVectors, labels)
-            iterP = list(unlabeledSet - negativeSet)
+            iterP = list(unlabeledExampleKeys - negativeSet)
             if len(iterP) == 0:
                 break
-            predictedLabels = iterSVM.predict(self.helperObj.dictOfFeaturesToList(featureDict, iterP))
+            predictedLabels = iterSVM.predict(self.helperObj.dictOfFeaturesToList(self.dict_X, iterP))
             newNegativeFeatures = set()
             for i in range(0, len(predictedLabels)):
                 if predictedLabels[i] == 0:
@@ -178,14 +254,14 @@ class Classifier:
                 self.logger.log("    iterP size: {0}".format(len(iterP)))
                 self.logger.log("    newNegatives size: {0}".format(len(newNegativeFeatures)))
 
-        self.trainedSVM = iterSVM
+        self.trainedClassifier = iterSVM
         end = time.mktime(time.localtime())
         self.logger.log("Elapsed training time: {0} minutes".format((end - start) / 60))
         return iterSVM
 
-    def _strongNegatives(self, unlabeledSet, positiveSet, featureDict):
-        posX = self.helperObj.dictOfFeaturesToList(featureDict, positiveSet)
-        unlabX = self.helperObj.dictOfFeaturesToList(featureDict, unlabeledSet)
+    def _strongNegatives(self, unlabeledSet, positiveSet):
+        posX = self.helperObj.dictOfFeaturesToList(self.dict_X, positiveSet)
+        unlabX = self.helperObj.dictOfFeaturesToList(self.dict_X, unlabeledSet)
 
         centroidPosX = Centroid().getCentroid(posX)
         indices = Centroid().getNFarthestPoints(unlabX, centroidPosX, len(posX))
--- src/run.py
+++ src/run.py
@@ -1,88 +1,28 @@
 from dataGrabber import dataGrabber, dataFileDescriptor
 from Classifier import Classifier
-from featureSelection import featureSelection
-from centroid import Centroid
 from helpers import helpers
 from logger import logger
-
-loggerObj = logger()
-helperObj = helpers(loggerObj)
-
-ripperRuleFileName = "/home/alex/KnowEng/data/RIPPER_RULES_GO40FILT2000_KEGGALL_x5POSITIVE.txt"
-matchFeatures = helperObj.getFeaturesFromRipperRules(ripperRuleFileName, ["go", "kegg"])
 
 filterTermsByCount = lambda feature, count: count >= 40 and count <= 2000
 filterByMatchFeatures = lambda feature, count: feature in matchFeatures
 
 breastCancerLabelFile = "/home/alex/KnowEng/data/VANTVEER_BREAST_CANCER_ESR1.CB.txt"
-keggDataFile = dataFileDescriptor("/home/alex/KnowEng/data/ENSG.kegg_pathway.txt",
-                                  filterByMatchFeatures)
-goDataFile = dataFileDescriptor("/home/alex/KnowEng/data/ENSG.go_%_evid.txt",
-                                filterByMatchFeatures)
+keggDataFile = dataFileDescriptor("/home/alex/KnowEng/data/ENSG.kegg_pathway.txt", filterByMatchFeatures)
+goDataFile = dataFileDescriptor("/home/alex/KnowEng/data/ENSG.go_%_evid.txt", filterByMatchFeatures)
 
+logOutputDir = "/home/alex/KnowEng/logs/svm/"
+
+loggerObj = logger(baseDir=logOutputDir)
+helperObj = helpers(loggerObj)
+
+ripperRuleFileName = "/home/alex/KnowEng/data/RIPPER_RULES_GO40FILT2000_KEGGALL_x5POSITIVE.txt"
+matchFeatures = helperObj.getFeaturesFromRipperRules(ripperRuleFileName, ["go", "kegg"])
 
 dataRetriever = dataGrabber(loggerObj)
-(featureVectorDict, labelDict, dataIndices) = dataRetriever.getData(breastCancerLabelFile, [goDataFile, keggDataFile])
-
-
-#dataRetriever.convertToCSV(featureVectorDict, labelDict, "test.csv")
-
-posX = helperObj.getFeaturesByLabel(featureVectorDict, labelDict, 1)
-unlabX = helperObj.getFeaturesByLabel(featureVectorDict, labelDict, 0)
-#
-#
-#
-# centroidPosX = Centroid().getCentroid(posX)
-# negIndices = Centroid().getNFarthestPoints(unlabX, centroidPosX, len(posX))
-#
-# negX = []
-# for i in negIndices:
-#      negX.append(unlabX[i])
-
-# newnewFeatureDict = {}
-# for k, v in newLabelDict.iteritems():
-#     if v == 1:
-#         newnewFeatureDict[k] = newFeatureDict[k]
-# for i in range(0, len(negX)):
-#     newnewFeatureDict[str(i)] = negX[i]
-#     newLabelDict[str(i)] = 0
-
-# dataRetriever.convertToCSV(newFeatureDict, newLabelDict, "out2.csv")
-
-
-# crossVal = []
-# delKeys = []
-#
-# count = 0
-# total = 0
-# for k, v in labelDict.iteritems():
-#     if v == 1:
-#         count += 1
-#         total += 1
-#     if count == 4:
-#         count = 0
-#         crossVal.append(featureVectorDict[k])
-#         delKeys.append(k)
-#         del featureVectorDict[k]
-# for key in delKeys:
-#     del labelDict[key]
+(featureVectorDict, labelDict, dataIndices) = dataRetriever.getData(breastCancerLabelFile, [keggDataFile, goDataFile])
 
 x = Classifier(loggerObj, verbose=True)
 
-# print("no gamma")
-# x.train(featureVectorDict, labelDict)
-# x.score(crossVal + unlabX, [1] * len(crossVal) + [0] * len(unlabX))
-#
-# print(".8 gamma")
-# x.train(featureVectorDict, labelDict, gamma=.8)
-# x.score(crossVal + unlabX, [1] * len(crossVal) + [0] * len(unlabX))
-#
-#print("number of hidden positives: {0}".format(len(crossVal)))
-
-x.trainSVM(featureVectorDict, labelDict, kernel="rbf", class_weight ='auto', gamma=0.0, degree=3)
-x.score(posX + unlabX, [1] * len(posX) + [0] * len(unlabX))
-
-#
-# print("linear")
-# x.train(featureVectorDict, labelDict, kernel="linear")
-# x.score(crossVal + unlabX, [1] * len(crossVal) + [0] * len(unlabX))
+x.trainSVM(featureVectorDict, labelDict, kCrossValPos=0, kCrossValNeg=0, kernel="rbf",
+           class_weight ='auto', gamma=0.0, degree=3)
+x.score()
